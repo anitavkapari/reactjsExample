af87f8c658d65302a8bb2e75fca84654613af163
anitavkapari <anitavkapari@gmail.com>
2019-07-20 15:13:13 +0530

    list display using by map function, onClick event, animation,function to component,cardView

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
-import React from 'react';  
+import React, {Component} from 'react';  
 import './Fruit.css';
 import FruitList from'./FruitList';
 import 'tachyons/'
-//class App extends React.Component() {  
- 
-    const App = () =>{
-          const fruitListarray =[
-              {
+class App extends Component{  
+ constructor(){
+     super();
+     this.state = {
+         name : "Welome to Annu"
+     }
+ }
+   // const App = () =>{
+    namechange(){
+        this.setState({
+            name : "Welome to Anita"
+
+        })
+    }
+        render (){
+            const fruitListarray =[
+                {
+                id:1,
+                img:'https://images.pexels.com/photos/39803/pexels-photo-39803.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 ',
+                name:'fruit',
+                work:'Apple',
+            },
+            {
               id:1,
-              img:'https://images.pexels.com/photos/39803/pexels-photo-39803.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 ',
-              name:'fruit',
-              work:'Apple',
+              img:'https://www.hindimeaning.com/wp-content/uploads/2012/12/carrots-vegetables.jpg',
+              name:'vegetables',
+              work:'Carrot',
           },
           {
-            id:1,
-            img:'https://www.hindimeaning.com/wp-content/uploads/2012/12/carrots-vegetables.jpg',
-            name:'vegetables',
-            work:'Carrot',
-        },
-        {
-            id:1,
-            img:'https://www.organicfacts.net/wp-content/uploads/2013/05/Vegetables4.jpg',
-            name:'fruits',
-            work:'fruit',
-        },
-        {
-            id:1,
-            img:'https://choosemyplate-prod.azureedge.net/sites/default/files/styles/food_gallery_colorbox__800x500_/public/myplate/Broccoli.jpeg?itok=aksUvoGw',
-            name:'Greenvegetables',
-            work:'Broccoli',
-        },
-        ]
+              id:1,
+              img:'https://www.organicfacts.net/wp-content/uploads/2013/05/Vegetables4.jpg',
+              name:'fruits',
+              work:'fruit',
+          },
+          {
+              id:1,
+              img:'https://choosemyplate-prod.azureedge.net/sites/default/files/styles/food_gallery_colorbox__800x500_/public/myplate/Broccoli.jpeg?itok=aksUvoGw',
+              name:'Greenvegetables',
+              work:'Broccoli',
+          },
+          ]
+          const arrayfruitcard = fruitListarray.map((fruitcard, i) =>{
+              return  <FruitList key={i} id='1' img={fruitListarray[i].img}
+              name={fruitListarray[i].name}
+               work={fruitListarray[i].work}/>
+          })
           return (  
-              <div>
-                  <h1 className='tc'> Welcome to Anita</h1>  
-                  <FruitList id='1' img={fruitListarray[0].img}
-                                    name={fruitListarray[0].name}
-                                     work={fruitListarray[0].work}/>
-
-                  <FruitList id='2'img={fruitListarray[1].img}
-                                    name={fruitListarray[1].name}
-                                     work={fruitListarray[1].work}/>
-
-                  <FruitList id='3'img={fruitListarray[2].img}
-                                    name={fruitListarray[2].name}
-                                     work={fruitListarray[2].work}/>
-
-                  <FruitList id='4' img={fruitListarray[3].img}
-                                    name={fruitListarray[3].name}
-                                     work={fruitListarray[3].work}/>
-
-                  <button className='ma4'> Subcribe</button>
+              <div className='mainpage'>
+                  <h1 className='tc'> {this.state.name}</h1>  
+                 {arrayfruitcard}
+                  <button className='ma4' onClick={()=> this.namechange()}> Subcribe</button>
                   </div>  
         )  
     }  
- 
+}
 export default App;  
--- src/FruitList.js
+++ src/FruitList.js
@@ -1,10 +1,10 @@
 import React from 'react';  
 const FruitList = (props) =>{
           return (  
-              <div className="fruitstyle ma4 bg-light-white dib ">
+              <div className="fruitstyle ma4 bg-light-white dib grow shadow-4 ">
                   <img className='pa4' src= {props.img}alt="fruit"/>  
-                  <h1 className=''>{props.name}</h1>  
-                  <p>{props.work}</p>
+                  <h1 className='ma2'>{props.name}</h1>  
+                  <p className='ma2'>{props.work}</p>
              </div>  
         )  
     }  
